55f6b603b10cecab1f44213eb6e30cd4a6a242eb
Yurii-Bodnar <yuriy.bodnar75@gmail.com>
2023-05-09 12:34:53 +0300

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/aria-proptypes */
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
